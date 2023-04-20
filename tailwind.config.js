0087661b67960f8fdeb150cbf5625f596a5af30a
Ying <ying@mintable.com>
2023-04-20 12:32:52 +0800

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        // mono: ["var(--font-xanh", ...fontFamily.mono],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "paper-texture":
+          "url('https://www.transparenttextures.com/patterns/little-pluses.png')",
+      },
+      colors: {
+        timber: "#D7D0C8",
+        bone: "#C8C6AF",
+        cambridge: "#95A78D",
+        "rose-brown": {
+          100: "#f8f3f2",
+          200: "#ebdcd8",
+          300: "#d7b8b2",
+          400: "#caa198",
+          500: "#aa7b71",
+          600: "#846058",
+          700: "#4c3732",
+          800: "#261b19",
+        },
+        bittersweet: "#F87666",
       },
     },
   },
   plugins: [],
-}
+};
