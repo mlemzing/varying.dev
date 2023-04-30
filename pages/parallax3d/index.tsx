438386457dd37f041c7af125e5eda4f1e78d26fc
Ying <ying1996@gmail.com>
2023-05-01 00:11:44 +0800

    move forest component into component folder

--- pages/parallax3d/index.tsx
+++ pages/parallax3d/index.tsx
@@ -1,5 +1,5 @@
-import { Canvas, useLoader, useFrame } from "@react-three/fiber";
-import Forest from "./forest";
+import { Canvas } from "@react-three/fiber";
+import Forest from "../../components/forest";
 export default function Parallax3D() {
   return (
     <div className="w-screen h-screen bg-white">
