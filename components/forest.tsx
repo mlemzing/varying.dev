c92f402197e201b024850b1766ee7dd426bc825f
Ying <ying1996@gmail.com>
2023-05-01 15:54:02 +0800

    add parallax layers

--- components/forest.tsx
+++ components/forest.tsx
@@ -1,42 +1,71 @@
+import { OrbitControls, useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import { useState } from "react";
 export default function Forest() {
+  const [pastForest, setPastForest] = useState(false);
+  const finalX = 18;
+  const [forest1, forest2, forest3, forest4, cloud1, cloud2, cloud3] =
+    useTexture([
+      "./forest/forest1.png",
+      "./forest/forest2.png",
+      "./forest/forest3.png",
+      "./forest/forest4.png",
+      "./forest/cloud1.png",
+      "./forest/cloud2.png",
+      "./forest/cloud3.png",
+    ]);
   useFrame((state, delta) => {
     state.camera.position.y +=
-      (state.mouse.y * 0.2 - state.camera.position.y) * 0.2;
-    if (state.camera.position.x < 8) {
+      (state.mouse.y * 0.5 - state.camera.position.y) * delta;
+    if (state.camera.position.x < finalX && !pastForest) {
       state.camera.position.x += delta;
     } else {
+      setPastForest(true);
       if (state.camera.position.z > -3) {
-        state.camera.position.z -= delta;
+        state.camera.position.z -= delta * 2;
+      }
+      if (pastForest) {
+        state.camera.position.x +=
+          (finalX - (state.camera.position.x + state.mouse.x * 0.75)) * delta;
       }
     }
   });
   return (
     <>
-      <mesh scale={5} position={[-2, 0, 1]}>
+      {/* <OrbitControls /> */}
+      <mesh scale={[20, 10, 10]} position={[0, 0, 0]}>
         <planeGeometry args={[1, 1, 20, 20]} />
-        <meshBasicMaterial color={"red"} wireframe />
+        <meshBasicMaterial map={forest1} transparent />
       </mesh>
-      <mesh scale={20} position={[-3, 0, -5]}>
+      <mesh scale={[20, 10, 10]} position={[0, 0, -0.5]}>
         <planeGeometry args={[1, 1, 20, 20]} />
-        <meshBasicMaterial color={"black"} wireframe />
+        <meshBasicMaterial map={forest2} transparent />
       </mesh>
-      <mesh scale={5}>
+      <mesh scale={[20, 10, 10]} position={[0, 0, -1]}>
         <planeGeometry args={[1, 1, 20, 20]} />
-        <meshBasicMaterial color={"blue"} wireframe />
+        <meshBasicMaterial map={forest3} transparent />
       </mesh>
-      <mesh scale={15} position={[2, 2, -2]}>
+      <mesh scale={[20, 10, 10]} position={[0, 0, -1.5]}>
+        <planeGeometry args={[1, 1, 20, 20]} />
+        <meshBasicMaterial map={forest4} transparent />
+      </mesh>
+      <mesh scale={10} position={[18, 0, -5]}>
+        <planeGeometry args={[1, 1, 20, 20]} />
+        <meshBasicMaterial map={cloud1} transparent />
+      </mesh>
+      <mesh scale={10} position={[18, 0, -6]}>
         <planeGeometry args={[1, 1, 10, 10]} />
-        <meshBasicMaterial color={"green"} wireframe />
+        <meshBasicMaterial color={"black"} wireframe />
+        <meshBasicMaterial map={cloud2} transparent />
       </mesh>
-      <mesh scale={20} position={[10, 0, -8]}>
+      <mesh scale={10} position={[18, 0, -7]}>
         <planeGeometry args={[1, 1, 20, 20]} />
-        <meshBasicMaterial color={"red"} wireframe />
+        <meshBasicMaterial map={cloud3} transparent />
       </mesh>
-      <mesh scale={12} position={[11, -3, -7]}>
+      {/* <mesh scale={12} position={[18, 0, -7]}>
         <planeGeometry args={[1, 1, 20, 20]} />
         <meshBasicMaterial color={"blue"} wireframe />
-      </mesh>
+      </mesh> */}
     </>
   );
 }
--- pages/parallax3d/index.tsx
+++ pages/parallax3d/index.tsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import Forest from "../../components/forest";
 export default function Parallax3D() {
   return (
-    <div className="w-screen h-screen bg-white">
+    <div className="w-screen h-screen bg-black">
       <Canvas>
         <Forest />
       </Canvas>
